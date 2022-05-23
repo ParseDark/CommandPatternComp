e80d027aea68f2df32ef7d9cf6a40c94cfcd4b75
hawei <13597213421@163.com>
2022-05-23 23:32:30 +0800

    feat: remove useless code.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import Head from "next/head";
 import Layout from "../../components/layout";
 import CommandPalette from "../../components/CommandPalette";
 
@@ -9,21 +6,6 @@ export default function FirstPost() {
   return (
     <Layout>
       <CommandPalette />
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>First Post</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Image
-        src="/images/profile.jpeg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-      />
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
     </Layout>
   );
 }
